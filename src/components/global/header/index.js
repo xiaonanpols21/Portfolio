848129c7fef0f9e192b74ea861c547da35e4d8f1
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-08-15 22:06:37 +0200

    header

--- src/components/global/header/index.js
+++ src/components/global/header/index.js
@@ -1,25 +1,47 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./header.module.scss";
 
 export default function Header() {
     return (
-        <header className={styles.header}>
-            <Image
-                src="/img/logo.svg"
-                width={120}
-                height={35}
-                alt="Xiao Logo"
-            />
-
-            <button>
+        <>
+            <header className={styles.header}>
                 <Image
-                    src="/img/icons/menu.svg"
-                    width={15}
-                    height={12}
-                    alt="Menu"
+                    src="/img/logo.svg"
+                    width={120}
+                    height={35}
+                    alt="Xiao Logo"
                 />
-            </button>
-        </header>
+
+                <button>
+                    <Image
+                        src="/img/icons/menu.svg"
+                        width={15}
+                        height={12}
+                        alt="Menu"
+                    />
+                </button>
+
+                <nav className={styles.nav}>
+                    <ul>
+                        <li><Link href="">Over mij</Link></li>
+                        <li><Link href="">Mijn projecten</Link></li>
+                        <li><Link href="">Ervaringen</Link></li>
+                        <li><Link href="">Contact</Link></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <section className={styles.openMenu}>
+                <h2 className="visually-hidden">Open menu</h2>
+                <ul>
+                    <li><Link href="">Over mij</Link></li>
+                    <li><Link href="">Mijn projecten</Link></li>
+                    <li><Link href="">Ervaringen</Link></li>
+                    <li><Link href="">Contact</Link></li>
+                </ul>
+            </section>
+        </>
     )
 }
