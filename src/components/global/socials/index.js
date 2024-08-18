be721a71a3ae266e4e3835c051b8f745455c0ce4
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-08-18 21:09:53 +0200

    socials gradients

--- src/components/global/socials/index.js
+++ src/components/global/socials/index.js
@@ -7,36 +7,42 @@ export default function Socials() {
     return (
         <ul className={styles.ul}>
             <li>
-                <Link href="">
-                    <Image
-                        src="/img/icons/github.svg"
-                        width={26}
-                        height={25}
-                        alt="Github"
-                    />
-                    <span>xiaonanpols21</span>
+                <Link href="https://github.com/xiaonanpols21">
+                    <figure>
+                        <Image
+                            src="/img/icons/github.svg"
+                            width={26}
+                            height={25}
+                            alt="Github"
+                        />
+                        <figcaption>xiaonanpols21</figcaption>
+                    </figure>
                 </Link>
             </li>
             <li>
-                <Link href="">
-                    <Image
-                        src="/img/icons/github.svg"
-                        width={26}
-                        height={25}
-                        alt="Github"
-                    />
-                    <span>xiaonanpols21</span>
+                <Link href="https://www.linkedin.com/in/xiao-nan-pols-b839951b5/">
+                    <figure>
+                        <Image
+                            src="/img/icons/linkedin.svg"
+                            width={26}
+                            height={25}
+                            alt="Github"
+                        />
+                        <figcaption>Xiao Nan Pols</figcaption>
+                    </figure>
                 </Link>
             </li>
             <li>
-                <Link href="">
-                    <Image
-                        src="/img/icons/github.svg"
-                        width={26}
-                        height={25}
-                        alt="Github"
-                    />
-                    <span>xiaonanpols21</span>
+                <Link href="https://www.instagram.com/xiaonanpols21/">
+                    <figure>
+                        <Image
+                            src="/img/icons/insta.svg"
+                            width={26}
+                            height={25}
+                            alt="Github"
+                        />
+                        <figcaption>xiaonanpols21</figcaption>
+                    </figure>
                 </Link>
             </li>
         </ul>
