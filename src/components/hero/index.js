3d711f6a336dad2f3337b6aad4b3af9d156890b8
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-09-01 16:59:27 +0200

    preview projecten eigen grid proberen

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -10,19 +10,22 @@ export default function Hero() {
     const imgData = data.headerImages;
 
     return (
-        <section className={styles.section}>
-            <h3 className="visually-hidden">Begin pagina</h3>
-            <DarkMode/>
-            <ScrollGallery data={imgData} />
-            <HeroGallery/>
+        <>
+            <section className={styles.section}>
+                <h3 className="visually-hidden">Begin pagina</h3>
+                <DarkMode/>
+                <ScrollGallery data={imgData} />
+                <HeroGallery/>
 
-            <div className={styles.headerContent}>
-                <h1>Creative developer</h1>
-                <p>lorem ipsum dolor sit</p>
-                <p>Lorem ipsum dolor sit amet consectetur. Orci velit id id volutpat. Feugiat pharetra non mi pharetra quis.</p>
-                <button>Mijn projecten</button>
-            </div>
-           <Projecten/>
-        </section>
+                <div className={styles.headerContent}>
+                    <h1>Creative developer</h1>
+                    <p>lorem ipsum dolor sit</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Orci velit id id volutpat. Feugiat pharetra non mi pharetra quis.</p>
+                    <button>Mijn projecten</button>
+                </div>
+            
+            </section>
+            <Projecten/>
+        </>
     )
 }
--- src/components/projecten/index.js
+++ src/components/projecten/index.js
@@ -4,50 +4,53 @@ import styles from "./projecten.module.scss";
 
 export default function Projecten() {
     return (
-        <section className={styles.section}>
-            <h2>Preview projecten</h2>
-            <section>
-                <article>
-                    <h3>TDMB Dating App</h3>
-                    <Image
-                        src="/img/thumbnail.png"
-                        width={500}
-                        height={500}
-                        alt="Picture of the author"
-                    />
+        <div className={styles.div}>
+            <section className={styles.section}>
+                <h2>Preview projecten</h2>
+                <section>
+                    <article>
+                        <h3>TDMB Dating App</h3>
+                        <Image
+                            src="/img/thumbnail.png"
+                            width={500}
+                            height={500}
+                            alt="Picture of the author"
+                        />
 
-                    <label className={styles.label}>
-                        <input type="checkbox"/>
-                        <span></span>
-                    </label>
-                </article>
-                <article>
-                    <h3>TDMB Dating App</h3>
-                    <Image
-                        src="/img/thumbnail.png"
-                        width={500}
-                        height={500}
-                        alt="Picture of the author"
-                    />
-                    <label className={styles.label}>
-                        <input type="checkbox"/>
-                        <span></span>
-                    </label>
-                </article>
-                <article>
-                    <h3>TDMB Dating App</h3>
-                    <Image
-                        src="/img/thumbnail.png"
-                        width={500}
-                        height={500}
-                        alt="Picture of the author"
-                    />
-                    <label className={styles.label}>
-                        <input type="checkbox"/>
-                        <span></span>
-                    </label>
-                </article>
+                        <label className={styles.label}>
+                            <input type="checkbox"/>
+                            <span></span>
+                        </label>
+                    </article>
+                    <article>
+                        <h3>TDMB Dating App</h3>
+                        <Image
+                            src="/img/thumbnail.png"
+                            width={500}
+                            height={500}
+                            alt="Picture of the author"
+                        />
+                        <label className={styles.label}>
+                            <input type="checkbox"/>
+                            <span></span>
+                        </label>
+                    </article>
+                    <article>
+                        <h3>TDMB Dating App</h3>
+                        <Image
+                            src="/img/thumbnail.png"
+                            width={500}
+                            height={500}
+                            alt="Picture of the author"
+                        />
+                        <label className={styles.label}>
+                            <input type="checkbox"/>
+                            <span></span>
+                        </label>
+                    </article>
+                </section>
             </section>
-        </section>
+        </div>
+        
     )
 }
